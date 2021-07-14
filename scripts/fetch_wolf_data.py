565bd6d0c058329974e4082d7c893c795f109b47
karltayeb <karl.tayeb7@gmail.com>
2021-07-14 15:54:41 -0500

    b

--- scripts/fetch_wolf_data.py
+++ scripts/fetch_wolf_data.py
@@ -1,7 +1,9 @@
 import pkg_resources
+import snakemake as snk
+
 data_path = pkg_resources.resource_filename("feems", "data/")
 
 input = ['{}wolvesadmix.{}'.format(data_path, ext) for ext in ['bed', 'bim', 'fam']]
 for i, o  in zip(input, snakemake.output):
     print('copying {} to {}'.format(i, o))
-    shell('cp {} {}'.format(i, o))
+    snk.shell('cp {} {}'.format(i, o))
