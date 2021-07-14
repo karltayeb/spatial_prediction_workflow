c37260e4fd500d45d1ee9e725dbb1ef28ca09e4b
karltayeb <karl.tayeb7@gmail.com>
2021-07-14 14:26:21 -0500

    get wolf data

---
+++ scripts/fetch_wolf_data.py
@@ -0,0 +1,7 @@
+import pkg_resources
+data_path = pkg_resources.resource_filename("feems", "data/")
+
+input = ['{}wolvesadmix.{}'.format(ext) for ext in ['bed', 'bim', 'fam']]
+for i, o  in zip(input, output):
+    print('copying {} to {}'.format(i, o))
+    shell('cp {} {}'.format(i, o))
