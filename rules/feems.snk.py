5122d2440bb9be5221daf5cf59b8271cc0226a4d
karltayeb <karl.tayeb7@gmail.com>
2021-07-20 14:24:26 -0500

    add gridsize

--- rules/feems.snk.py
+++ rules/feems.snk.py
@@ -11,16 +11,14 @@ def get_translated(wildcards):
 rule feems_initialize_graph:
     input:
         multiext('data/{prefix}', '.bed', '.bim', '.fam', '.coord', '.outer'),
-        grid_path = 'data/grids/grid_100.shp'
+        grid_path = 'data/grids/grid_{gridsize}.shp'
     output:
-        'output/{prefix}/feems/init_sp_graph.pkl'
+        'output/{prefix}/feems/sp_graph_grid_{gridsize}.pkl'
     params:
         data_path = 'data/{prefix}',
         translated = get_translated
     conda:
         '../envs/feems.yaml'
-    resources:
-        ntasks_per_node=4
     script:
         '../scripts/feems_init_sp_graph.py'
 
@@ -30,10 +28,10 @@ rule feems_split_nodes:
     create coord files that do not have locations for samples from same node
     """
     input:
-        sp_graph='output/{prefix}/feems/init_sp_graph.pkl',
+        sp_graph='output/{prefix}/feems/sp_graph_grid_{gridsize}.pkl',
         coord='data/{prefix}.coord'
     output:
-        expand('output/{prefix}/node_splits/{split}.coord',
+        expand('output/{prefix}/node_splits/grid_{gridsize}_split_{split}.coord',
             split=range(10), allow_missing=True)
     params:
         nsplits = lambda wildcards, output: len(output)
@@ -45,10 +43,10 @@ rule feems_split_nodes:
 
 rule feems_run_split_nodes:
     input:
-        sp_graph='output/{prefix}/feems/init_sp_graph.pkl',
-        coord='output/{prefix}/node_splits/{split}.coord'
+        sp_graph='output/{prefix}/feems/sp_graph_grid_{gridsize}.pkl',
+        coord='output/{prefix}/node_splits/grid_{gridsize}_split_{split}.coord'
     output:
-        'output/{prefix}/feems/node_splits/{split}.{fit}.{predict}.feems.pkl'
+        'output/{prefix}/feems/node_splits/feems_{fit}_{predict}_grid_{gridsize}_split_{split}.feems.pkl'
     conda:
         '../envs/feems.yaml'
     script:
