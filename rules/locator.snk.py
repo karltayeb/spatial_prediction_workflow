afe794b29b7e55780288f000c71dfd63e91d86dc
karltayeb <karl.tayeb7@gmail.com>
2021-07-21 06:28:13 -0500

    locator coords

--- rules/locator.snk.py
+++ rules/locator.snk.py
@@ -3,28 +3,37 @@ import pickle
 # node2sample = pickle.load(open('/Users/karltayeb/Research/spatial_prediction/feems/feems/data/node2sample.pkl', 'rb'))
 # observed_nodes = [k for k in node2sample if len(node2sample[k]) > 0]
 
-rule prep_coord:
+rule prep_coord_for_locator:
     """
-    just copy popres data to data repo
+    just copy coords to locator output file,
+    reformat coords for locator
     """
     input:
         'data/{prefix}.fam',
-        'data/{prefix}_splits/{split}.coord'
-    output: 'data/{prefix}_splits/{split}.locator.coord'
-
+        directory('output/{prefix}/feems/grid_{gridsize}/leave_node_out/coord')
+    output:
+        directory('output/{prefix}/locator/grid_{gridsize}/leave_node_out/coord/')
     run:
         import pandas as pd
-        coord = pd.read_csv(input[1], index_col=False, header=None, sep='\s')
-        fam = pd.read_csv(input[0], index_col=False, header=None, sep='\s')
-        fam['sampleID']=fam.apply(lambda x:'%s_%s' % (x[0],x[1]),axis=1)
-        meta = pd.concat([coord, fam['sampleID']], axis=1).iloc[:, :4]
-        meta.columns = ['x', 'y', 'sampleID']
-        meta.to_csv(output[0], sep='\t', index=None)
+        import os
+        from glob import glob
+
+        os.makedirs(output[0], exist_ok=True)
+        paths = glob(input.coord_dir + '/*')
+
+        for p in paths:
+            p_out = '{}/{}'.format(output[0], p.split('/')[-1])
+            coord = pd.read_csv(p, index_col=False, header=None, sep='\s')
+            fam = pd.read_csv(input[0], index_col=False, header=None, sep='\s')
+            fam['sampleID']=fam.apply(lambda x:'%s_%s' % (x[0],x[1]),axis=1)
+            meta = pd.concat([coord, fam['sampleID']], axis=1).iloc[:, :4]
+            meta.columns = ['x', 'y', 'sampleID']
+            meta.to_csv(p_out, sep='\t', index=None)
 
 rule run_locator:
     input:
         vcf='data/{prefix}.vcf',
-        loc='data/{prefix}_splits/{split}.locator.coord'
+        loc_dir=directory('output/{prefix}/locator/grid_{gridsize}/leave_node_out/coord/')
     output:
         directory('output/{prefix}/locator/splits/{split}')
     params:
