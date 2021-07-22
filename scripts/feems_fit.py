bf882c3624b04621fbe2d320868b8fda706dfb4a
karltayeb <karl.tayeb7@gmail.com>
2021-07-22 12:11:22 -0500

    regularize frequencies

--- scripts/feems_fit.py
+++ scripts/feems_fit.py
@@ -10,9 +10,32 @@ elif snakemake.wildcards.fit == 'feems':
 else:
     assert(False)
 
+
+def regularize_frequencies(self, alpha=0):
+    """
+    update attributes frequencies and S in self
+    compute weighted average of global freuqencies
+    and and observed node frequencies
+    """
+    alpha = 0.5
+
+    n = self.n_samples_per_obs_node_permuted
+    mu = self.mu
+    scale = np.sqrt(mu * (1-mu))
+
+    f = self.frequencies * scale
+    f_reg = (1 - alpha / n)[:, None] * f + (alpha / n)[:, None] * mu * 2
+    
+    self.frequencies = f_reg / scale
+    self.S = self.frequencies @ self.frequencies.T / self.n_snps
+
 sp_graph = pickle.load(open(snakemake.input.sp_graph, 'rb'))
 coord = pd.read_csv(snakemake.input.coord, sep='\t', header=None)
 
+if 'alpha' in snakemake.wildcards.reg:
+    alpha = float(snakemake.wildcards.reg.split('-')[-1])
+    regularize_frequencies(sp_graph, alpha)
+    
 results = predict_held_out_nodes(
         sp_graph, coord,
         predict_type=snakemake.wildcards.predict,
