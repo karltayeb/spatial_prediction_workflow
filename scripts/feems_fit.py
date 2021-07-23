30fc1b67761332391a44c926df096384662e0401
karltayeb <karl.tayeb7@gmail.com>
2021-07-23 12:19:38 -0500

    b

--- scripts/feems_fit.py
+++ scripts/feems_fit.py
@@ -12,6 +12,9 @@ else:
     assert(False)
 
 
+predict_type = snakemake.wildcards.predict
+fit_feems = fit
+
 def regularize_frequencies(self, alpha):
     """
     update attributes frequencies and S in self
@@ -43,9 +46,61 @@ if 'alpha' in snakemake.wildcards.reg:
     sp_graph = regularize_frequencies(sp_graph, alpha)
 
 print(sp_graph.S[0])
-results = predict_held_out_nodes(
-        sp_graph, coord,
-        predict_type=snakemake.wildcards.predict,
-        fit_feems = fit
-    )
+
+
+sample_idx = query_node_attributes(sp_graph, 'sample_idx')
+permuted_idx = query_node_attributes(sp_graph, "permuted_idx")
+sp_graph.fit_null_model()
+
+# deepcopy doesn't like sp_graph.factor...
+sp_graph.factor = None
+
+# remove test demes from training
+n = sp_graph.sample_pos.shape[0]
+split = ~np.isnan(coord.iloc[:, 0])
+sp_graph_train, sp_graph_test = train_test_split(sp_graph, split)
+
+
+test_sample_idx = query_node_attributes(sp_graph_test, 'sample_idx')
+test_node2sample = {i: test_sample_idx[i]
+    for i in range(len(test_sample_idx))
+    if len(test_sample_idx[i]) > 0}
+test_nodes = list(test_node2sample.keys())
+print('fit feems w/o observations @ node: {}'.format(test_nodes))
+
+if fit_feems:
+    # TODO use fit_kwargs
+    # sp_graph_train.fit(**fit_kwargs)
+    sp_graph_train.fit(lamb=2., verbose=True)
+
+
+# get genotypes of test deme
+g = sp_graph.genotypes
+g[~np.isclose(g, g.astype(int))] = np.nan
+
+# predict
+if predict_type == 'point':
+    z, post_mean = predict_deme_point_mu(g, sp_graph_train)
+
+# predict
+if predict_type == 'trunc':
+    z, post_mean = predict_deme_trunc_normal_mu(g, sp_graph_train)
+
+results = {
+    'post_assignment': z[~split],
+    'w': sp_graph_train.w,
+    'w0': sp_graph_train.w0,
+    's2': sp_graph_train.s2,
+    #'post_mean': post_mean, # compute posterior mean
+    'map_coord': sp_graph.node_pos[permuted_idx][z.argmax(1)],
+    'pred_idx': np.where(~split)[0],
+    'test_nodes': test_nodes
+}
+
+
+# results = predict_held_out_nodes(
+#         sp_graph, coord,
+#         predict_type=snakemake.wildcards.predict,
+#         fit_feems = fit
+#     )
 pickle.dump(results, open(snakemake.output[0], 'wb'))
